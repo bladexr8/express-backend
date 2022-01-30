49afc7454f224ebe29986d7e842d46d17fb6a443
Stephen Moss <bladexr8@gmail.com>
2022-01-30 22:55:41 +1100

    add express routing

--- index.js
+++ index.js
@@ -6,16 +6,30 @@ const emails = require('./fixtures/emails');
 
 let app = express();
 
+let getUsersRoute = (req, res) => {
+    res.send(users);
+}
+
+let getEmailsRoute = (req, res) => {
+    res.send(emails);
+}
+
+let noRouteFound = (req, res) => {
+    let route = req.method + ' ' + req.url;
+    res.end('You asked for ' + route);
+}
+
+let routes = {
+    'GET /users': getUsersRoute,
+    'GET /emails': getEmailsRoute,
+};
+
+
 app.use((req, res) => {
     let route = req.method + ' ' + req.url;
+    let handler = routes[route] || noRouteFound;
 
-    if (route === 'GET /users') {
-        res.send(users);
-    } else if (route === 'GET /emails') {
-        res.send(emails);
-    } else {
-        res.end('You asked for ' + route);
-    }
+    handler(req, res);
     
 });
 
