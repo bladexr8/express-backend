39c4d46156347b2c0ab8f6421557657a0b63f467
Stephen Moss <bladexr8@gmail.com>
2022-01-30 22:05:57 +1100

    basic express server

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+
+const users = require('./fixtures/users');
+const emails = require('./fixtures/emails');
+
+let app = express();
+
+app.use((req, res) => {
+    let route = req.method + ' ' + req.url;
+
+    if (route === 'GET /users') {
+        res.send(users);
+    } else if (route === 'GET /emails') {
+        res.send(emails);
+    } else {
+        res.end('You asked for ' + route);
+    }
+    
+});
+
+app.listen(3000);
