0654213909a86cad57ae4add6279cbc9f8ea524c
Stephen Moss <bladexr8@gmail.com>
2022-01-31 19:32:43 +1100

    finished chapter 04

--- src/express/routes/emails.js
+++ src/express/routes/emails.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const readBody = require('../../lib/read-body');
+const generateId = require('../../lib/generate-id');
 const emails = require('../fixtures/emails');
 
 let getEmailsRoute = (req, res) => {
@@ -23,20 +24,47 @@ let createEmailRoute = async (req, res) => {
     console.log(body.toString());
 
     // create email
-    let newEmail = JSON.parse(body);
+    let newEmail = {
+        ...JSON.parse(body), id: generateId()
+    }
     emails.push(newEmail);
 
     // generate response
     res.status(201);
     res.send({
         "status": "OK",
-        "message": "Email Received"
+        "message": "Email Created",
+        "notes": newEmail
     });
+}
+
+let updateEmailRoute = async (req, res) => {
+    let body = await readBody(req);
+    let email = emails.find(email => email.id === req.params.id);
+    Object.assign(email, JSON.parse(body));
+    res.status(200);
+    res.send({
+        "status": "OK",
+        "message": "Email Updated",
+        "notes": email
+    })
+}
+
+let deleteEmailRoute = (req, res) => {
+    let index = emails.findIndex(email => email.id === req.params.id);
+    emails.splice(index, 1);
+    res.status(204);
+    res.send({
+        "status": "OK",
+        "message": "Email Deleted",
+    })
 }
 
 let emailsRouter = express.Router();
 emailsRouter.get('/', getEmailsRoute);
 emailsRouter.get('/:id', getEmailRoute);
 emailsRouter.post('/', createEmailRoute);
+emailsRouter.patch('/:id', updateEmailRoute);
+emailsRouter.delete('/:id', deleteEmailRoute);
 
 module.exports = emailsRouter;
